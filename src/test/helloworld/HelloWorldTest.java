a2f38bbff3d168f08e10c5c3600c7157e3915179
FilipAlberius <filip@alberius.se>
2019-07-10 16:09:56 +0200

    Project structure in place

---
+++ src/test/helloworld/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+    @Test
+    void test(){
+        assertEquals(1,1);
+    }
+}
