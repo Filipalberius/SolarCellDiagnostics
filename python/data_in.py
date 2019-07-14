3d6605403314be959210ab726e7a4b1e22d5fad4
Filipalberius <36191024+Filipalberius@users.noreply.github.com>
2019-07-14 23:00:32 +0200

    can get data on specific line of file

--- python/data_in.py
+++ python/data_in.py
@@ -1,13 +1,34 @@
-import sys
+def get_line(index):
+    file = open('produktionsdata.csv')
+
+    return file.readlines()[index].split(';')
 
 
-def init(stdin):
-    test = stdin.readline().split()
-    print(test)
+def print_data(line):
+    identity = line[0]
+    date_format = line[1]
+    date = line[2]
+    q_code = line[3]
+    period = line[4]
+    unit = line[5]
+
+    data = line[10:]
+
+    print("Id: " + identity + ". Date: " + date)
+    print("Production per hour:")
+
+    i = 0
+    a = 0
+    for datum in data:
+        if i % 2 == 0:
+            print("Hour " + str(a) + ": " + datum + " " + unit)
+            a += 1
+        i += 1
 
 
 def main():
-    init(sys.stdin)
+    line = get_line(0)
+    print_data(line)
 
 
 if __name__ == '__main__':
