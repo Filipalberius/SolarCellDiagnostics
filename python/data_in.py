82f6898b1d01fdd561092715bd7e810284f3f6bc
FilipAlberius <filip@alberius.se>
2019-07-23 22:41:39 +0200

    refactor

--- python/data_in.py
+++ python/data_in.py
@@ -17,7 +17,7 @@ def read(path):
 
 
 # Prints the data on a given line.
-def data_toarray(line):
+def get_values(line):
 
     values = list()
     data = line[10:]
@@ -28,24 +28,18 @@ def data_toarray(line):
             values.append(value)
         i += 1
 
-    return_string = "[";
-
-    for number in values:
-        return_string += (number + ";")
-    return_string = return_string[:-1]
-    return_string += "]"
-    print(return_string)
+    return values
 
 
 # Finds the interval of entries containing the correct identity and dates.
-def get_data(file, identity, open_date, close_date):
+def get_data(file, identity, start_date, end_date):
     data = read(file)
     entries = list()
     for line in data:
         entry = line.split(';')
-        if entry[0] == identity:
-            if open_date <= int(entry[2]) <= close_date:
-                entries.append(entry)
+        entry_id, date = entry[0], int(entry[2])
+        if entry_id == identity and start_date <= date <= end_date:
+            entries.append(entry)
     return entries
 
 
@@ -58,15 +52,12 @@ def dialogue():
     return identity, start, end
 
 
-def print_data(entries):
-    for entry in entries:
-        data_toarray(entry)
-
-
 def main():
     identity, start, end = "734012530000024571", 201811290000, 201812220000
     entries = get_data('produktionsdata.csv', identity, start, end)
-    print_data(entries)
+
+    for e in entries:
+        print(get_values(e))
 
 
 if __name__ == '__main__':
